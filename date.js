2802195fb2f81bae1aab88bf662264885b9dd58d
Mousmi Suryawanshi <61198246+mousmi2112@users.noreply.github.com>
2020-09-29 09:40:06 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+
+module.exports.getDate=getDate;
+
+function getDate(){
+
+var today=new Date();
+    
+    var options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+
+    let day=today.toLocaleDateString("en-US",options);
+    return day;
+}
